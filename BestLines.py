cffa849775b1d6a38fedf7c0b331797b97fa64c6
Leo Adavirat <leoadavirat@gmail.com>
2021-01-21 10:53:25 +0700

    hh:

--- BestLines.py
+++ BestLines.py
@@ -1,2 +1,16 @@
+import re
 def isPrime(n):
 	return re.match(r'^1?$|^(11+?)\1+$', '1' * n) == None
+def Primes(n):
+    prime = [True for i in range(n + 1)]
+    p = 2
+    while (p * p <= n):
+        if (prime[p] == True):
+            for i in range(p * 2, n + 1, p):
+                prime[i] = False
+        p += 1
+    prime[0]= False
+    prime[1]= False
+    for p in range(n + 1):
+        if prime[p]:
+            print(p)
---
+++ testmodule.py
@@ -0,0 +1,3 @@
+import BestLines
+n = int(input("Please enter a number:  "))
+print('Is this number a prime number?  ' + str(BestLines.isPrime(n)))
